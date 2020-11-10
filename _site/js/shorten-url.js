370b149ce92c75b3d9134af6ceec88c2a8d5600f
Seunghwan <seunghwan@ab180.co>
2020-11-10 16:39:40 +0900

    notion deeplink 생성

--- _site/js/shorten-url.js
+++ _site/js/shorten-url.js
@@ -1,8 +1,20 @@
 function UrlShorten() {
     var suffixUrl = "https://tinyurl.com/api-create.php?url=";
+    
+    /* input에서 value를 가져옵니다 */
     var longUrl = document.getElementById('long_url').value;
-    var requestUrl = suffixUrl + longUrl;
-    fetch(requestUrl)
+
+    /* longUrl의 Scheme을 구합니다 */
+    var scheme = longUrl;
+    scheme = scheme.split(".")[1];
+    
+    /* 구한 Scheme이 notion일 경우 notion 딥링크 url로 변경하여 요청합니다 */
+    if (scheme = "notion") {
+        var notionUrl = longUrl;
+        notionUrl = notionUrl.split("://")[1]
+        var requestUrl = suffixUrl + "notion://" + notionUrl;
+
+        fetch(requestUrl)
         .then((response) => {
             return response.text();
         })
@@ -12,6 +24,22 @@ function UrlShorten() {
             document.getElementById('submit').setAttribute("value", "Copy");
             document.getElementById('submit').setAttribute("onclick", "CopyUrlToClipboard()");
         })
+    }
+    /* 이외의 경우 받은 url을 그대로 사용해 요청합니다 */
+    else {
+            var requestUrl = suffixUrl + longUrl;
+        
+            fetch(requestUrl)
+                .then((response) => {
+                    return response.text();
+                })
+                .then((myJson) => {
+                    console.log(myJson);
+                    document.getElementById('long_url').value = myJson;
+                    document.getElementById('submit').setAttribute("value", "Copy");
+                    document.getElementById('submit').setAttribute("onclick", "CopyUrlToClipboard()");
+                })
+        }
     return;
 };
 
