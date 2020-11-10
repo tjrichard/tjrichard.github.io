dae2af2528d4a0b7d0c0b3ab953f174990602648
Seunghwan <seunghwan@ab180.co>
2020-11-10 14:00:23 +0900

    add tinyurl shortner at connect-180

---
+++ js/shorten-url.js
@@ -0,0 +1,33 @@
+function UrlShorten() {
+    var suffixUrl = "http://tinyurl.com/api-create.php?url=";
+    var longUrl = document.getElementById('long_url').value;
+    var requestUrl = suffixUrl + longUrl;
+    fetch(requestUrl)
+        .then((response) => {
+            return response.text();
+        })
+        .then((myJson) => {
+            console.log(myJson);
+            document.getElementById('long_url').value = myJson;
+            document.getElementById('submit').setAttribute("value", "Copy");
+            document.getElementById('submit').setAttribute("onclick", "CopyUrlToClipboard()");
+        })
+    return;
+};
+
+function CopyUrlToClipboard() {
+    /* Get the text field */
+    var copyText = document.getElementById('long_url');
+
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+    // copyText.blur(); // 선택된 것을 다시 선택안된것으로 바꿈니다.
+
+    /* Change Button to Shorten again */
+    document.getElementById('submit').setAttribute("value", "Shorten");
+    document.getElementById('submit').setAttribute("onclick", "UrlShorten()");
+};
