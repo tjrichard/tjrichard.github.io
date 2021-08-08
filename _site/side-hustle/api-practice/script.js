a8eb82c17af92f98007174b88ce58c6b3a0b2042
tjrichard <tjrichard91@gmail.com>
2021-08-09 00:01:27 +0900

    이벤트 추가

--- _site/side-hustle/api-practice/script.js
+++ _site/side-hustle/api-practice/script.js
@@ -10,6 +10,12 @@
 /*
 =================== Common functoins ===================
 */
+
+!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="QmVYyLedpWWw5IGRhwvxW0DhG1Py1BCG";analytics.SNIPPET_VERSION="4.13.2";
+analytics.load("QmVYyLedpWWw5IGRhwvxW0DhG1Py1BCG");
+analytics.page();
+}}();
+
 Storage.prototype.setObj = function(key, obj) {
     return this.setItem(key, JSON.stringify(obj))
 };
@@ -146,6 +152,11 @@ function getApi_ipInfo() {
     })
     .then(response => response.json())
     .then(json => {
+        var currentUrl = window.location.href;
+        analytics.track("Get IP Info",{
+            "url": currentUrl,
+            "target": json
+            })
         console.log(json);
         getCurrentTime();
         // appendList(json);
@@ -163,6 +174,11 @@ function getApi_currentWeather() {
     })
     .then(response => response.json())
     .then(json => {
+        var currentUrl = window.location.href;
+        analytics.track("Get Current Weather",{
+        "url": currentUrl,
+        "target": json
+        })
         var lat = Object.values(json)[8];
         var lon = Object.values(json)[9];
         let key = "18ad4f9360c88c9ebd053c46af11eb08"
