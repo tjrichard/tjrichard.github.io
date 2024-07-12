f23f33c56a1b9093feb254415c47e7be0304fdac
Ryan-Jang <tjrichard91@gmail.com>
2024-07-12 17:07:17 +0900

    Update supabase.ts

--- _includes/supabase.ts
+++ _includes/supabase.ts
@@ -1,5 +1,5 @@
 
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://lqrkuvemtnnnjgvptnlo.supabase.co'
+const supabaseUrl = process.env.SUPABASEURL
 const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
