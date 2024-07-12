edd6a36817ad301e2cf5d59f0dfb1d02c0ff8afd
Ryan-Jang <tjrichard91@gmail.com>
2024-07-12 17:57:49 +0900

    init emoji translator

--- _includes/supabase.ts
+++
@@ -1,5 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = process.env.SUPABASEURL
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
---
+++ _site/side-hustle/emoji-translator/script.js
@@ -0,0 +1,38 @@
+import { createClient } from '@supabase/supabase-js';
+
+// 환경 변수가 제대로 설정되었는지 확인
+const supabaseUrl = process.env.SUPABASEURL;
+const supabaseKey = process.env.SUPABASE_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+    console.error("Supabase URL or Key is not defined in environment variables.");
+}
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+const emojiShortName = async () => {
+    let emoji = document.getElementById("submittedEmoji").value;
+    
+    const { data, error } = await supabase
+      .from("emojis")
+      .select("shortname")
+      .eq("emoji", emoji);
+  
+    if (error) {
+      console.error(error);
+      return null;
+    }
+    return data;
+};
+
+async function getEmoji_shortName() {
+    const result = await emojiShortName(); 
+    console.log(result);
+    let resultArea = document.getElementById("apiResponse");
+    resultArea.innerText = result;
+}
+
+// DOM이 로드된 후 이벤트 리스너를 추가
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById('submit').addEventListener('click', getEmoji_shortName);
+});
